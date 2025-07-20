31a46009e026de37b05da9d68231c7606c6b1b2a
telegina-mm <teleginamaria988@gmail.com>
2025-07-20 10:44:11 +0300

    my solution

--- include/hybrid.h
+++ include/hybrid.h
@@ -1,6 +1,25 @@
 
-// declare Hybrid class here
 
+#include <string>
 #include "human.h"
 
-// your code here
+class Hybrid : public Human {
+  private:
+    bool vampire_power;
+    bool werewolf_power;
+    int rage_level;
+
+  public:
+    using Human::Human;
+    Hybrid(const std::string& name, int age);
+
+    Hybrid& activateVampirePower();
+    Hybrid& activateWerewolfPower();
+
+    Hybrid& increaseRage();
+    void checkRage();
+
+    bool hasVampirePower() const;
+    bool hasWerewolfPower() const;
+    int getRageLevel() const;
+};
